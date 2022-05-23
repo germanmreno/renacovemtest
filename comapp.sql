2aa89e6758e8c2099b17d550e9e138ffacc86381
Soporte CVM <soportecvm12@gmail.com>
2022-05-23 14:47:20 -0400

    BD.

--- comapp.sql
+++ comapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-05-2022 a las 16:57:00
+-- Tiempo de generación: 23-05-2022 a las 20:46:24
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `companies` (
   `guid` varchar(36) NOT NULL,
+  `numregistro` varchar(30) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `rif` varchar(25) NOT NULL,
   `numalianza` varchar(25) NOT NULL,
@@ -55,10 +56,16 @@ CREATE TABLE `companies` (
   `promediooro` varchar(300) NOT NULL,
   `promedioganancia` varchar(20) NOT NULL,
   `porcentajecompra` varchar(20) NOT NULL,
-  `status` varchar(15) NOT NULL DEFAULT 'enespera',
   `createdAt` date NOT NULL,
   `updatedAt` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `companies`
+--
+
+INSERT INTO `companies` (`guid`, `numregistro`, `nombre`, `rif`, `numalianza`, `direccionfiscal`, `estado`, `municipio`, `parroquia`, `representante`, `telefonorepresentante`, `correorepresentante`, `cedularepresentante`, `rumrepresentante`, `tipoactividad`, `actividadminera`, `descripcionactminera`, `nombreencargado`, `cedulaencargado`, `numempleados`, `nombreempleados`, `cedulaempleados`, `cargoempleados`, `medidacomercio`, `inventario`, `promediooro`, `promedioganancia`, `porcentajecompra`, `createdAt`, `updatedAt`) VALUES
+('d4e20437-d6fb-4c32-a03d-418d835210c8', '391831246', 'Comercializadora de Oro', '13213123123', '123', '123', 'Miranda', 'Acevedo', 'Aragüita', '12312', '13123', '123132', '123312', '12313', '\"Compra/Venta,Análisis/Fundición\"', '\"Diamante,Coltán\"', '123', 'sddsa', 'sadsad', '1', 'sasad', 'assadsad', 'asds', '130', '\"Espectómetro,CajaFuerte\"', '123', '123', '213', '2022-05-19', '2022-05-19');
 
 -- --------------------------------------------------------
 
@@ -82,7 +89,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`guid`, `nombreusuario`, `permisos`, `contraseña`, `correo`, `telefono`, `createdAt`, `updatedAt`) VALUES
-('91df651e-01d5-44b6-a483-13f30811fcc5', 'germancvm', 'usuario', '$2a$10$.hRiObZ26LHrZek9IsmxcunJXczefTTJyAVooNFud/ZPjVw7ZkFHy', 'german@cvm.com', '04140210028', '2022-05-19', '2022-05-19');
+('0e13fa3a-aa6a-493c-9b95-1bf4e883c484', 'germancvm', 'usuario', '$2a$10$xKrxtIT03CCNDbE7BfRs..A/4AcymADZ1n9Ikqt.CW6J/.a7yo2Wq', 'german@gmail.com', '04140210028', '2022-05-20', '2022-05-20'),
+('91df651e-01d5-44b6-a483-13f30811fcc5', 'germancvm', 'usuario', '$2a$10$.hRiObZ26LHrZek9IsmxcunJXczefTTJyAVooNFud/ZPjVw7ZkFHy', 'german@cvm.com', '04140210028', '2022-05-19', '2022-05-19'),
+('a2953845-963e-4675-a8bf-7bca2fca97df', 'yinniercvm', 'usuario', '$2a$10$62Yo2a4.E2b8.DBOjawJg.OkiKYzJbyYok8t7O12Z5Fn1y55IN2he', 'cvm@gmail.com', '02120210028', '2022-05-19', '2022-05-19'),
+('d4e20437-d6fb-4c32-a03d-418d835210c8', 'jesuscvm', 'usuario', '$2a$10$Gla1vE9p9w2jJPEAh72CLuHpmlqdRtEU9HSLJPtoUZRQZ48LgFFWm', 'cvm', '12321', '2022-05-19', '2022-05-19');
 
 --
 -- Índices para tablas volcadas
